3ce2688a90f1e8a6287c7e803a4ce2eee76bfaf2
BT_1N3_27 <kfang9357@bths.edu>
2019-01-07 08:17:43 -0500

    adding utilities

--- src/com/company/BubbleSort.java
+++ src/com/company/BubbleSort.java
@@ -1,12 +1,13 @@
 package com.company;
-
+import static com.company.SortingUtilities.isSorted;
+import static com.company.SortingUtilities.swap;
 
 public class BubbleSort {
     public static void bubbleSort(int[] arr)
     {
         int len = arr.length;
 
-        while (checkList(arr) )
+        while (isSorted(arr) )
         {
             for (int i = 0; i < len - 1; i++)
             {
@@ -17,29 +18,6 @@ public class BubbleSort {
             }
         }
     }
-
-
-
-    public static void swap (int[]arr, int i, int j)
-    {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-
-    public static boolean checkList (int[] arr)
-    {
-        int len = arr.length - 1;
-        for (int pos = 0; pos < len; pos++)
-        {
-            if (arr[pos]>arr[pos+1])
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
 
     //int [] testArr = {8,6,7,5,3,0,9,10,1,2,3};
 
