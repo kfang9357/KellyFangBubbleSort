b2c16004b2efd1bd927a36990310f1ef4ecdb2ae
kfang9357 <43207499+kfang9357@users.noreply.github.com>
2019-01-10 21:53:52 -0500

    converted Bubble Sort to String

--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -31,16 +31,16 @@ public class Runner {
         {System.out.print(num+" ");}
         System.out.println();
 
-
+        long time2 = System.nanoTime();
         InPlaceSorts.selectionSort(testArr2);
-
+        time2 = System.nanoTime() - time2;
 
         System.out.print("After: ");
         for (int num:testArr2) {
             System.out.print(num + " ");
         }
         System.out.println();
-
+        System.out.println("Time taken: "+time2+" nanoseconds");
 
         /*****************************************************/
 
@@ -51,16 +51,16 @@ public class Runner {
         {System.out.print(num+" ");}
         System.out.println();
 
-
+        long time3 = System.nanoTime();
         InPlaceSorts.insertionSort(testArr3);
-
+        time3 = System.nanoTime() - time3;
 
         System.out.print("After: ");
         for (int num:testArr3) {
             System.out.print(num + " ");
         }
         System.out.println();
-
+        System.out.println("Time taken: "+time3+" nanoseconds");
         }
 
     }
